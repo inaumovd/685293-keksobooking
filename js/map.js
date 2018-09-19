3ce460f0f4a335ef08e5da69b6773933422a1511
Dmitry <inaumovd@gmail.com>
2018-09-19 16:41:02 +0800

    Небольшие правки

--- js/map.js
+++ js/map.js
@@ -210,7 +210,7 @@ var activatePage = function () {
 disableInputs(inputs, true);
 addressInput.value = parseInt(mainPin.style.left, 10) + ', ' + parseInt(mainPin.style.top, 10);
 
-mainPin.addEventListener('mouseup', function (evt) {
+mainPin.addEventListener('mouseup', function () {
   activatePage();
   pins = getItemsList(8);
   appendPins(pins);
