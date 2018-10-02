f6f6498e4abde621f5b2510670bed03013dad5fc
Dmitry <inaumovd@gmail.com>
2018-10-02 21:52:03 +0800

    filterjs

---
+++ js/filter.js
@@ -0,0 +1,16 @@
+'use strict';
+
+(function () {
+
+var housingTypeSelect = document.querySelector('#housing-type');
+
+housingTypeSelect.addEventListener('change', function () {
+  console.log(window.pin.pinsData);
+});
+
+  // window.filter = {
+
+  // };
+
+})();
+
