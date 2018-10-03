3931af07759834a874fb4e098d61e24f7d5ba38c
Dmitry <inaumovd@gmail.com>
2018-10-03 21:29:33 +0800

    7-1

--- js/filter.js
+++ js/filter.js
@@ -5,12 +5,12 @@
 var housingTypeSelect = document.querySelector('#housing-type');
 
 housingTypeSelect.addEventListener('change', function () {
-  console.log(window.pin.pinsData);
+  console.log(window.pin.getPinsData());
 });
 
-  // window.filter = {
+  window.filter = {
 
-  // };
+  };
 
 })();
 
